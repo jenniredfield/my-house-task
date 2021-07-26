4fb38aa833ed5fb21031eaf73f88c80b6a67a70e
Jennifer Bird <jenniredfield@hotmail.com>
2021-07-26 18:21:28 +0100

    Updated tasks to show one week before and a few in advance

--- src/App.js
+++ src/App.js
@@ -1,45 +1,63 @@
-import moment from "moment";
-import "./App.css";
+import moment from 'moment';
+import { calculateHouseMateWithTask, createListWithDates } from './lib/utils';
+import './App.css';
 
 function App() {
-  const startingDate = "28/12/2020";
-  const tasks = ["Kitchen", "Toilets", "Corridors", "Bins"];
-  const housemates = ["Lyndon", "Jen", "Anais", "Martim"];
-  const startDate = moment(startingDate, "DD/MM/YYYY");
+  const startingDate = '28/12/2020';
+  const tasks = ['Kitchen', 'Toilets', 'Corridors', 'Bins & Supplies'];
+  const housemates = ['Lyndon', 'Jen', 'Anais', 'Martim'];
+  const startDate = moment(startingDate, 'DD/MM/YYYY');
   const now = moment();
-  const diff = now.diff(startDate, "weeks");
+  const numberOfWeeksElapsed = now.diff(startDate, 'weeks');
 
-  const housematesWithTasks = housemates.map((housemate, i) => {
-    const housematesLength = housemates.length;
-    const currNum = (diff % housematesLength) + i;
-    return {
-      housemate,
-      task:
-      currNum >= 4
-          ? tasks[currNum % housematesLength]
-          : tasks[(diff % housematesLength) + i],
-    };
-  });
+  console.log(
+    'calc',
+    calculateHouseMateWithTask(
+      housemates,
+      tasks,
+      startDate,
+      numberOfWeeksElapsed
+    )
+  );
 
-  const weekStartingDate = moment(startingDate, "DD/MM/YYYY").add(String(diff), 'weeks').format('DD/MM');
-  const weekEndingDate = moment(startingDate, "DD/MM/YYYY").add(String(diff), 'weeks').add('6', 'days').format('DD/MM');
+  console.log(
+    'list',
+    createListWithDates(housemates, tasks, startDate, numberOfWeeksElapsed)
+  );
+  const listWithHouseMateTasks = createListWithDates(
+    housemates,
+    tasks,
+    startDate,
+    numberOfWeeksElapsed
+  );
 
   return (
     <div className="App">
       <header>
         <h1>Your task of the week</h1>
-        <h2>{`${weekStartingDate} - ${weekEndingDate}`}</h2>
       </header>
       <div className="app-content">
         <div className="app-tasks">
-          {housematesWithTasks.map((h) => {
-            return (
-              <div className="app-task-column" key={h.task}>
-                <div className="app-task-row">{h.housemate}</div>
-                <div className="app-task-row">{h.task}</div>
-              </div>
-            );
-          })}
+          {listWithHouseMateTasks.map(
+            ({ weekDateDetails, houseMatesWithTasks }) => {
+              return (
+                <div className="app-task">
+                  <h2 className="app-task-date-title">{`${weekDateDetails.weekStartDate} - ${weekDateDetails.weekEndDate}`}</h2>
+
+                  <div className="app-task-details-container">
+                    {houseMatesWithTasks.map((h) => {
+                      return (
+                        <div className="app-task-column" key={h.task}>
+                          <div className="app-task-row">{h.housemate}</div>
+                          <div className="app-task-row">{h.task}</div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            }
+          )}
         </div>
       </div>
     </div>
